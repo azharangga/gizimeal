faf70b4a22fa21185ed5132533574566d1252d0b
azharangga <azharanggakusuma1@.com>
2026-05-28 09:11:13 +0700

    fix error glintch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
